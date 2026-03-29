4da5ac598e5f3bae2df4f18eb095ac7e2ae0a5f0
lishangbu <shangshili@hotmail.com>
2026-03-29 13:05:27 +0800

    fix(config): 取消打包分组

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      port: 5174,
+      port: 5173,
       strictPort: true,
       proxy: {
         '/api': {
@@ -28,51 +28,8 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      // 抑制naive-ui的chunk过大警告
-      chunkSizeWarningLimit: 1200,
       rolldownOptions: {
         output: {
-          codeSplitting: {
-            groups: [
-              {
-                name: 'chroma-js',
-                test: /\/chroma-js/,
-              },
-              {
-                name: 'es-toolkit',
-                test: /\/es-toolkit/,
-              },
-              {
-                name: 'naive-ui',
-                test: /\/naive-ui/,
-              },
-              {
-                name: 'vue-draggable-plus',
-                test: /\/vue-draggable-plus/,
-              },
-              {
-                name: 'vueuse',
-                test: /\/vueuse/,
-              },
-              {
-                name: 'vue-router',
-                test: /\/vue-router/,
-              },
-              {
-                name: 'pinia',
-                test: /\/pinia/,
-              },
-              {
-                name: 'axios',
-                test: /\/axios/,
-              },
-              {
-                name: 'vue',
-                test: /\/vue/,
-              },
-            ],
-          },
-
           assetFileNames: (asset) => {
             const notHash = ['topography.svg', 'texture.png', 'noise.png']
             if (asset.names?.some((name) => notHash.includes(name))) {
